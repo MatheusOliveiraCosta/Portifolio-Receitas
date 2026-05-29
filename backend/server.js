5589467a799faec9d9ccff8ca5b316d3ed660814
Matheus Oliveira Costa <matheus14lon@gmail.com>
2026-05-29 14:54:29 -0300

    Implementação de documentação Swagger

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); 
 const conectarBanco = require('./database'); 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const app = express();
 
@@ -36,3 +38,34 @@ app.use('/api/relatorios', relatorioRoutes);
 
 const publicRoutes = require('./routes/publicRoutes');
 app.use('/api/publico', publicRoutes);
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0', // Versão padrão atual
+        info: {
+            title: 'API - Painel de Receitas',
+            version: '1.0.0',
+            description: 'Documentação da API de Receitas Colaborativas',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000', // URL base do seu Node
+            },
+        ],
+        // 2. Isso aqui avisa o Swagger que seu sistema usa Token JWT!
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                }
+            }
+        }
+    },
+    // 3. Onde o Swagger deve procurar as rotas para documentar?
+    apis: ['./routes/*.js'], 
+};
+
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
